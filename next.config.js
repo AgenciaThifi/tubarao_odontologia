3734778d808e2e43bf1d5faa3de67fecfc386e1a
DouglasiOSDeveloper <douguerpw2@hotmail.com>
2025-05-20 11:53:24 -0300

    changes 5

--- next.config.js
+++ next.config.js
@@ -1,8 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  trailingSlash: true,
   images: {
     unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'via.placeholder.com',
+      }
+    ],
+  },
+  typescript: {
+    ignoreBuildErrors: true,
   },
 };
 
