aa8a539db7c263938ced63e90c2387eccfd6f37c
DouglasiOSDeveloper <douguerpw2@hotmail.com>
2025-05-20 15:16:06 -0300

    changes 14

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
-  assetPrefix: './',
+  // só adiciona './' antes dos assets quando estiver em produção
+  assetPrefix: isProd ? './' : '',
   basePath: '',
   images: {
     unoptimized: true,
@@ -14,7 +17,7 @@ const nextConfig = {
       {
         protocol: 'https',
         hostname: 'via.placeholder.com',
-      }
+      },
     ],
   },
   typescript: {
